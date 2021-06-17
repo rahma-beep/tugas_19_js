f32c30b9cba705c2aabfefd154fb41fa1d3012d2
Nur Rahmadani <nagamas.rahma@gmail.com>
2021-06-17 17:18:24 +0800

    TUGAS 19 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas19.js
@@ -0,0 +1,21 @@
+function tugas19() {
+  var kata = "Semangat...";
+  console.log(kata.repeat(5));
+
+}
+tugas19()
+
+function tugas19dua() {
+  var bintang = "*";
+  console.log(bintang.repeat(5));
+  console.log(bintang.repeat(25));
+  console.log(bintang.repeat(45));
+  console.log(bintang.repeat(65));
+  console.log(bintang.repeat(95));
+  console.log(bintang.repeat(65));
+  console.log(bintang.repeat(45));
+  console.log(bintang.repeat(25));
+  console.log(bintang.repeat(5));
+}
+
+tugas19dua()
